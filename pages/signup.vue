21f424cffb00e66cb2840673cfd8975fab3b1792
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2024-05-10 04:17:15 +0900

    Set data binding for signup.vue

--- pages/signup.vue
+++ pages/signup.vue
@@ -6,9 +6,15 @@
       <v-form
         v-model="isValid"
       >
-        <user-form-name />
-        <user-form-email />
-        <user-form-password />
+        <user-form-name
+          :name.sync="params.user.name"
+        />
+        <user-form-email
+          :email.sync="params.user.email"
+        />
+        <user-form-password
+          :password.sync="params.user.password"
+        />
         <v-btn
           :disabled="!isValid"
           block
@@ -18,6 +24,7 @@
           登録する
         </v-btn>
       </v-form>
+      {{ params }}
       <nuxt />
     </template>
   </user-form-card>
@@ -30,7 +37,8 @@ export default {
   layout: 'before-login',
   data () {
     return {
-      isValid: false
+      isValid: false,
+      params: { user: { name: '', email: '', password: '' } }
     }
   }
 }
