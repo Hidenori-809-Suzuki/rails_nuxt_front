390d09c2915f0ec7552cb80472768bee7e09a931
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2024-05-16 11:50:12 +0900

    Add logout process

--- store/index.js
+++ store/index.js
@@ -54,9 +54,11 @@ export const actions = {
     commit('serProjectList', projects)
   },
   getCurrentProject ({ state, commit }, params) {
-    const id = Number(params.id)
-    const currentProject =
-      state.project.list.find(project => project.id === id) || null
+    let currentProject = null
+    if (params && params.id) {
+      const id = Number(params.id)
+      currentProject = state.project.list.find(project => project.id === id) || null
+    }
     commit('setCurrentProject', currentProject)
   },
   getCurrentUser ({ commit }, user) {
