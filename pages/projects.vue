24f23358f339a9989e22937bbdd51751e6b6ee2d
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2024-05-16 10:45:09 +0900

    Add middleware for get project list

--- pages/projects.vue
+++ pages/projects.vue
@@ -135,6 +135,7 @@
 import homeImg from '~/assets/images/logged-in/home.png'
 export default {
   layout: 'logged-in',
+  middleware: ['get-project-list'],
   data () {
     return {
       homeImg,
